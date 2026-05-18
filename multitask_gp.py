92b2a32b5bf013e83f81674b47a0b543a6c33738
P2SAMAPA <pranabpattanaik@gmail.com>
2026-05-18 11:00:59 +0800

    Update multitask_gp.py

--- multitask_gp.py
+++ multitask_gp.py
@@ -2,46 +2,56 @@ import torch
 import gpytorch
 import numpy as np
 
-class MultitaskGPModel(gpytorch.models.ExactGP):
-    def __init__(self, train_x, train_y, likelihood, num_tasks):
+class IndependentGPModel(gpytorch.models.ExactGP):
+    def __init__(self, train_x, train_y, likelihood):
         super().__init__(train_x, train_y, likelihood)
-        self.mean_module = gpytorch.means.Mean()
+        self.mean_module = gpytorch.means.ConstantMean()
         self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
-        self.task_covar = gpytorch.kernels.IndexKernel(num_tasks=num_tasks, rank=num_tasks)
-
-    def forward(self, x, i):
+    def forward(self, x):
         mean = self.mean_module(x)
         covar = self.covar_module(x)
-        covar = covar.mul(self.task_covar(i))
         return gpytorch.distributions.MultivariateNormal(mean, covar)
 
-def train_mtgp(train_X, train_Y, num_tasks, n_inducing=None, lr=0.01, iterations=500):
-    n = train_X.shape[0]
-    task_indices = torch.arange(num_tasks).repeat(n, 1).t().reshape(-1)
-    X_repeated = train_X.repeat_interleave(num_tasks, dim=0)
-    Y_flat = train_Y.T.reshape(-1, 1)
-    likelihood = gpytorch.likelihoods.GaussianLikelihood()
-    model = MultitaskGPModel(X_repeated, Y_flat.squeeze(), likelihood, num_tasks)
-    model.train()
-    likelihood.train()
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
-    for i in range(iterations):
-        optimizer.zero_grad()
-        output = model(X_repeated, task_indices)
-        loss = -mll(output, Y_flat.squeeze())
-        loss.backward()
-        optimizer.step()
-        if (i+1) % 100 == 0:
-            print(f"    Iter {i+1}/{iterations}, loss: {loss.item():.4f}")
-    return model, likelihood
+def train_individual_gps(train_X, train_Y, num_tasks, lr=0.01, iterations=500):
+    """
+    train_X: torch tensor (n, d)
+    train_Y: torch tensor (n, num_tasks)
+    Returns lists of models and likelihoods.
+    """
+    models = []
+    likelihoods = []
+    for task in range(num_tasks):
+        y_task = train_Y[:, task]
+        likelihood = gpytorch.likelihoods.GaussianLikelihood()
+        model = IndependentGPModel(train_X, y_task, likelihood)
+        model.train()
+        likelihood.train()
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+        mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
+        for i in range(iterations):
+            optimizer.zero_grad()
+            output = model(train_X)
+            loss = -mll(output, y_task)
+            loss.backward()
+            optimizer.step()
+            if (i+1) % 100 == 0:
+                print(f"    Task {task+1} iter {i+1}/{iterations}, loss: {loss.item():.4f}")
+        models.append(model)
+        likelihoods.append(likelihood)
+    return models, likelihoods
 
-def predict(model, likelihood, test_X, num_tasks):
-    test_indices = torch.arange(num_tasks)
-    X_test = test_X.repeat(num_tasks, 1)
-    model.eval()
-    likelihood.eval()
-    with torch.no_grad():
-        pred = likelihood(model(X_test, test_indices))
-        mean = pred.mean.cpu().numpy()
-    return mean
+def predict_individual(models, likelihoods, test_X, num_tasks):
+    """
+    Returns array of predicted means (length num_tasks).
+    """
+    preds = []
+    for task in range(num_tasks):
+        model = models[task]
+        likelihood = likelihoods[task]
+        model.eval()
+        likelihood.eval()
+        with torch.no_grad():
+            pred = likelihood(model(test_X))
+            mean = pred.mean.cpu().numpy()
+            preds.append(mean.item())
+    return np.array(preds)
