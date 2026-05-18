fccc0ffeb7f3a64d6537200b74a4737497a4e3c2
P2SAMAPA <pranabpattanaik@gmail.com>
2026-05-18 10:54:33 +0800

    Update multitask_gp.py

--- multitask_gp.py
+++ multitask_gp.py
@@ -2,15 +2,9 @@ import torch
 import gpytorch
 import numpy as np
 
-class MultitaskGPModel(gpytorch.models.ApproximateGP):
-    def __init__(self, num_tasks, inducing_points):
-        variational_distribution = gpytorch.variational.CholeskyVariationalDistribution(
-            inducing_points.size(-2), batch_shape=torch.Size([num_tasks])
-        )
-        variational_strategy = gpytorch.variational.VariationalStrategy(
-            self, inducing_points, variational_distribution
-        )
-        super().__init__(variational_strategy)
+class MultitaskGPModel(gpytorch.models.ExactGP):
+    def __init__(self, train_x, train_y, likelihood, num_tasks):
+        super().__init__(train_x, train_y, likelihood)
         self.mean_module = gpytorch.means.Mean()
         self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
         self.task_covar = gpytorch.kernels.IndexKernel(num_tasks=num_tasks, rank=num_tasks)
@@ -21,18 +15,17 @@ class MultitaskGPModel(gpytorch.models.ApproximateGP):
         covar = covar.mul(self.task_covar(i))
         return gpytorch.distributions.MultivariateNormal(mean, covar)
 
-def train_mtgp(train_X, train_Y, num_tasks, n_inducing=100, lr=0.01, iterations=500):
+def train_mtgp(train_X, train_Y, num_tasks, n_inducing=None, lr=0.01, iterations=500):
     n = train_X.shape[0]
     task_indices = torch.arange(num_tasks).repeat(n, 1).t().reshape(-1)
     X_repeated = train_X.repeat_interleave(num_tasks, dim=0)
     Y_flat = train_Y.T.reshape(-1, 1)
-    inducing_points = train_X[:n_inducing]
-    model = MultitaskGPModel(num_tasks, inducing_points)
     likelihood = gpytorch.likelihoods.GaussianLikelihood()
+    model = MultitaskGPModel(X_repeated, Y_flat.squeeze(), likelihood, num_tasks)
     model.train()
     likelihood.train()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    mll = gpytorch.mlls.VariationalELBO(likelihood, model, num_data=len(Y_flat))
+    mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
     for i in range(iterations):
         optimizer.zero_grad()
         output = model(X_repeated, task_indices)
@@ -51,7 +44,4 @@ def predict(model, likelihood, test_X, num_tasks):
     with torch.no_grad():
         pred = likelihood(model(X_test, test_indices))
         mean = pred.mean.cpu().numpy()
-        # Ensure it's a 1D array of length num_tasks
-        if mean.size == 1 and num_tasks > 1:
-            mean = np.full(num_tasks, mean.item())
     return mean
